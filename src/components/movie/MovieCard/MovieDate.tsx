a879cd2b0c131caaaefa4d483f6d5f10bc627dd8
Reload0213 <dldudwn7227@gmail.com>
2025-03-06 19:08:16 +0900

    Fix: 스타일 정리

--- src/components/movie/MovieCard/MovieDate.tsx
+++ src/components/movie/MovieCard/MovieDate.tsx
@@ -1,10 +1,15 @@
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
+
 interface MovieDateProps {
   date: string;
 }
 
 const MovieDate = ({ date }: MovieDateProps) => {
+	const formatted = format(new Date(date), "yyyy년 MM월 dd일", { locale: ko });
+
 	return (
-		<div className="text-white mt-4">{date}</div>
+		<div className="text-caption text-gray-400 text-right">개봉일: {formatted}</div>
 	);
 };
 
